d54c8d9e1681e4b06d609d54953ae228abab7fef
JVocoloiD <61340635+JVocoloiD@users.noreply.github.com>
2020-03-25 15:30:30 -0700

    Add files via upload

--- DuckbilledPlatypus.java
+++ DuckbilledPlatypus.java
@@ -1,5 +1,13 @@
-
 public class DuckbilledPlatypus extends Mammal {
 
+    public static final BirthType DEFAULT_BIRTHTYPE = BirthType.LAYS_EGGS;
 
+    public DuckbilledPlatypus(int id, String name) {
+        super(id, name, DEFAULT_BIRTHTYPE);
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription() + "\tDuckbilled Platypus";
+    }
 }
